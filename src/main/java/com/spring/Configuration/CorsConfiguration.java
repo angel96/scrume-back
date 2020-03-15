ebe8a8c6b7d918c3b86e297750a7cc566b8675a5
Angel Delgado <angeldelgadoluna@gmail.com>
2020-03-15 13:09:35 +0100

    feat(configuration): Incluida clase CorsConfiguration
    Permitira la comunicación entre servidores.

---
+++ src/main/java/com/spring/Configuration/CorsConfiguration.java
@@ -0,0 +1,22 @@
+package com.spring.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH");
+			}
+		};
+	}
+
+}
