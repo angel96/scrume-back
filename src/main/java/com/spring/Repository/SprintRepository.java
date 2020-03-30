d3c6f154cfc6ae480b0f440d9e0138bb6907d435
davcamalv <dcamalv@gmail.com>
2020-03-30 13:38:04 +0200

    fix(sprint): arreglado test de workspace por cambio en sprintRepository

--- src/main/java/com/spring/Repository/SprintRepository.java
+++ src/main/java/com/spring/Repository/SprintRepository.java
@@ -15,6 +15,6 @@ public interface SprintRepository extends AbstractRepository<Sprint> {
 	@Query("select s.id from Sprint s where s.project = ?1 order by s.startDate asc")
 	List<Integer> findBySprintsOrdered(Project idProject);
 
-	@Query("select count(s) from Sprint s where s.id != ?4 and s.project = ?3 and ((s.startDate <= ?1 and s.endDate > ?1) or (s.startDate <= ?2 and s.endDate > ?2) or (s.startDate >= ?1 and s.endDate <= ?2))")
+	@Query("select count(s) from Sprint s where s.id != ?4 and s.project = ?3 and ((s.startDate <= ?1 and s.endDate >= ?1) or (s.startDate <= ?2 and s.endDate >= ?2) or (s.startDate >= ?1 and s.endDate <= ?2))")
 	Integer areValidDates(Date startDate, Date endDate, Project project, Integer idSprint);
 }
