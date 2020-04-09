0441504b5479c3a7b41dcafe598fecfd7675ad56
davcamalv <dcamalv@gmail.com>
2020-04-09 20:03:00 +0200

    feat(notification): añadido get put y list para el admin

--- src/main/java/com/spring/Repository/NotificationRepository.java
+++ src/main/java/com/spring/Repository/NotificationRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.spring.Model.Notification;
+import com.spring.Model.Sprint;
 import com.spring.Model.Team;
 import com.spring.Model.User;
 
@@ -14,5 +15,8 @@ public interface NotificationRepository extends AbstractRepository<Notification>
 
 	@Query("select n from Notification n where n.sprint.project.team = ?2 and (n.user = null or n.user = ?1) and n.date < current_timestamp")
 	Collection<Notification> listByUser(User user, Team team);
+	
+	@Query("select n from Notification n where n.sprint = ?1 and n.date > current_timestamp and n.user = null")
+	Collection<Notification> listAllInactiveNotifications(Sprint sprint);
 
 }
